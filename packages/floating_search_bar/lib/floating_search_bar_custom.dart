c722c4b500ac90acfadd51fe77f4507bf8e3fd8d
Mario Chamuty <mario@chamuty.com>
2019-05-14 15:29:09 +0200

    searchbarchanges

---
+++ packages/floating_search_bar/lib/floating_search_bar_custom.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+import 'ui/sliver_search_bar.dart';
+export 'ui/sliver_search_bar.dart';
+
+class FloatingSearchBarCustom extends StatelessWidget {
+  FloatingSearchBar({
+    this.body,
+    this.drawer,
+    this.trailing,
+    this.leading,
+    this.endDrawer,
+    this.controller,
+    this.onChanged,
+    this.title,
+    this.decoration,
+    this.onTap,
+    this.baseStyle,
+    @required List<Widget> children,
+  }) : _childDelagate = SliverChildListDelegate(
+          children,
+        );
+
+  final Widget leading, trailing, body, drawer, endDrawer;
+
+  final SliverChildDelegate _childDelagate;
+
+  final TextEditingController controller;
+
+  final ValueChanged<String> onChanged;
+
+  final InputDecoration decoration;
+  final TextStyle baseStyle;
+
+  final VoidCallback onTap;
+
+  /// Override the search field
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverFloatingBar(
+            leading: leading,
+            floating: true,
+            backgroundColor: Colors.transparent,
+            title: title ??
+                TextField(
+                  controller: controller,
+                  style: baseStyle,
+                  decoration: decoration ??
+                      InputDecoration.collapsed(
+                        hintText: "Search...",
+                      ),
+                  autofocus: false,
+                  onChanged: onChanged,
+                  onTap: onTap,
+                ),
+            trailing: trailing,
+          );
+  }
+}
